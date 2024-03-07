befb7447514f4e6ab755a09436b3c89e54533a36
Sufyan Ahmed <sufiyan03313074760>
2024-03-07 15:03:17 +0500

    search in array & serach check and show

--- script.js
+++ script.js
@@ -17,4 +17,36 @@ function ShowImages(){
     document.querySelector(".container").innerHTML = clutter
 }
 
+function SearchFunc(){
+    var input = document.querySelector("#searchinput")
+    input.addEventListener("focus", function(){
+        document.querySelector(".overlay").style.display = "block"
+    })
+
+    input.addEventListener("blur", function(){
+        document.querySelector(".overlay").style.display = "none"
+    })
+
+    input.addEventListener("input" , function(){
+        console.log(input.value)
+        let filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        console.log(filteredArray)
+        var clutter = ""
+        filteredArray.forEach(function(obj){
+            clutter +=`<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+
+        })
+        document.querySelector(".searchdata").style.display = "block"
+        document.querySelector(".searchdata").innerHTML = clutter
+    })
+
+   
+}
+
+
+
+SearchFunc()
 ShowImages()
